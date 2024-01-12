956110f3440cc3c1a8e06b1e3b58f8a6c14c0627
moulhatah99 <moulhatahmed@email.com>
2024-01-12 16:43:22 +0100

    tp2 partie 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LivresModule } from './livres/livres.module';
-import { AuteursModule } from './auteurs/auteurs.module';
+import { AuteursModule } from './auteurs/auteurs.module'; 
 
 @Module({
-  imports: [LivresModule, AuteursModule],
+  imports: [LivresModule, AuteursModule], 
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
